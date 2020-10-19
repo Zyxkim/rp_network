87d11b33184578073adb74edb2d419196f531e90
zyxkim <zyxkim.bmstu@gmail.com>
2020-10-19 16:14:46 +0300

    Added fragment for Login.

---
+++ UrBook/app/src/main/java/com/adzteam/urbook/registration/LoginFragment.java
@@ -0,0 +1,25 @@
+package com.adzteam.urbook.registration;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.adzteam.urbook.R;
+
+public class LoginFragment extends Fragment {
+
+    public LoginFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
+}
