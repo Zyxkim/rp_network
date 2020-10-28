96afd109801bfd3c45c7cdc9e7ed5af9258ca76b
DanielaObushcharova <obush41@mail.ru>
2020-10-28 16:44:12 +0300

    added registration and authentication by email using Firebase

--- UrBook/app/src/main/java/com/adzteam/urbook/registration/LoginFragment.java
+++ UrBook/app/src/main/java/com/adzteam/urbook/registration/LoginFragment.java
@@ -2,16 +2,28 @@ package com.adzteam.urbook.registration;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.adzteam.urbook.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginFragment extends Fragment {
+
+    EditText mEmail, mPassword;
+    Button mLoginBtn;
+    FirebaseAuth fAuth;
 
     public LoginFragment() {
         // Required empty public constructor
@@ -22,7 +34,33 @@ public class LoginFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_login, container, false);
+        mEmail = view.findViewById(R.id.email);
+        mPassword = view.findViewById(R.id.password);
+        mLoginBtn = view.findViewById(R.id.loginBtn);
         TextView goRegister = view.findViewById(R.id.goRegister);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        mLoginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Валидация
+
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                fAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getActivity(), "logged in successfully", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(getActivity(), "error " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+            }
+        });
 
         goRegister.setOnClickListener(new View.OnClickListener() {
             @Override
--- UrBook/app/src/main/java/com/adzteam/urbook/registration/RegistrationFragment.java
+++ UrBook/app/src/main/java/com/adzteam/urbook/registration/RegistrationFragment.java
@@ -2,16 +2,27 @@ package com.adzteam.urbook.registration;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.adzteam.urbook.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegistrationFragment extends Fragment {
+    EditText mUserName, mEmail, mPassword, mConfirmPassword;
+    Button mRegisterBtn;
+    FirebaseAuth fAuth;
 
     public RegistrationFragment() {
         // Required empty public constructor
@@ -22,7 +33,40 @@ public class RegistrationFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_registration, container, false);
+        mUserName = view.findViewById(R.id.name);
+        mEmail = view.findViewById(R.id.email);
+        mPassword = view.findViewById(R.id.password);
+        mConfirmPassword = view.findViewById(R.id.confirmPassword);
+        mRegisterBtn = view.findViewById(R.id.registerBtn);
         TextView goLogin = view.findViewById(R.id.goLogin);
+
+        fAuth = FirebaseAuth.getInstance();
+
+        mRegisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //валидация данных
+
+                String email = mEmail.getText().toString().trim();
+                String password = mPassword.getText().toString().trim();
+
+                fAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            if (getActivity() != null) {
+                                Toast.makeText(getActivity(), "new account created", Toast.LENGTH_SHORT).show();
+                                ((AuthActivity) getActivity()).replaceWithLoginFragment();
+                            }
+                        } else {
+                            if (getActivity() != null) {
+                                Toast.makeText(getActivity(), "error " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                    }
+                });
+            }
+        });
 
         goLogin.setOnClickListener(new View.OnClickListener() {
             @Override
