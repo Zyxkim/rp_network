a6c9f6f058956b3df7fe27ec00078246578432bc
alina-torr <toropchina_a1@mail.ru>
2020-11-18 21:32:49 +0300

    for test google auth

--- UrBook/app/src/main/java/com/adzteam/urbook/registration/RegistrationFragment.java
+++ UrBook/app/src/main/java/com/adzteam/urbook/registration/RegistrationFragment.java
@@ -1,32 +1,46 @@
 package com.adzteam.urbook.registration;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.adzteam.urbook.R;
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.common.api.ApiException;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.auth.AuthCredential;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
 
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegistrationFragment extends Fragment {
+    private static final String TAG = "GoogleActivity";
+    private static final int RC_SIGN_IN = 9001;
     EditText mUserName, mEmail, mPassword, mConfirmPassword;
     TextInputLayout mEmailBox, mPasswordBox, mConfirmPasswordBox, mUserBox;
     Button mRegisterBtn;
+    ImageView mGoogleRegisterBtn;
     FirebaseAuth fAuth;
     boolean mIsVerified = false;
 
@@ -48,6 +62,55 @@ public class RegistrationFragment extends Fragment {
     }
 
     @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                GoogleSignInAccount account = task.getResult(ApiException.class);
+                Log.d(TAG, "firebaseAuthWithGoogle:" + account.getId());
+                //firebaseAuthWithGoogle(account.getIdToken());
+            } catch (ApiException e) {
+                // Google Sign In failed, update UI appropriately
+                Log.w(TAG, "Google sign in failed", e);
+                // [START_EXCLUDE]
+                //updateUI(null);
+                // [END_EXCLUDE]
+            }
+        }
+    }
+
+    /*private void firebaseAuthWithGoogle(String idToken) {
+        // [START_EXCLUDE silent]
+        //showProgressBar();
+        // [END_EXCLUDE]
+        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithCredential:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithCredential:failure", task.getException());
+                            Snackbar.make(mBinding.mainLayout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // [START_EXCLUDE]
+                        hideProgressBar();
+                        // [END_EXCLUDE]
+                    }
+                });
+    }*/
+
+    @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
@@ -61,6 +124,8 @@ public class RegistrationFragment extends Fragment {
         mPasswordBox = view.findViewById(R.id.password_text);
         mConfirmPasswordBox = view.findViewById(R.id.confirm_password_text);
         mUserBox = view.findViewById(R.id.name_text);
+        mGoogleRegisterBtn = view.findViewById(R.id.googleAuth);
+
         TextView goLogin = view.findViewById(R.id.goLogin);
 
         fAuth = FirebaseAuth.getInstance();
@@ -121,6 +186,24 @@ public class RegistrationFragment extends Fragment {
                         }
                     });
                 }
+            }
+        });
+
+
+
+        mGoogleRegisterBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                        .requestIdToken(getString(R.string.default_web_client_id))
+                        .requestEmail()
+                        .build();
+                GoogleSignInClient mGoogleSignInClient = GoogleSignIn.getClient(getActivity(), gso);
+
+                Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+                Log.i("aaa", String.valueOf(signInIntent));
+                startActivityForResult(signInIntent, RC_SIGN_IN);
             }
         });
 
