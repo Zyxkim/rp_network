9fc157a9833f8f34bfc556c9726e1511684a9fd8
zyxkim <zyxkim.bmstu@gmail.com>
2020-11-29 14:37:25 +0300

    #4 added "Add Room" dialog to Rooms fragment.

--- UrBook/app/src/main/java/com/adzteam/urbook/general/GeneralActivity.java
+++ UrBook/app/src/main/java/com/adzteam/urbook/general/GeneralActivity.java
@@ -3,14 +3,15 @@ package com.adzteam.urbook.general;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.adzteam.urbook.R;
-import com.google.android.material.bottomnavigation.BottomNavigationView;
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
+
+import com.adzteam.urbook.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
 
 public class GeneralActivity extends AppCompatActivity {
 
@@ -23,9 +24,6 @@ public class GeneralActivity extends AppCompatActivity {
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
 
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_rooms, R.id.navigation_profile)
                 .build();
@@ -33,5 +31,4 @@ public class GeneralActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupWithNavController(navView, navController);
     }
-
 }
--- UrBook/app/src/main/java/com/adzteam/urbook/general/ui/rooms/RoomsFragment.java
+++ UrBook/app/src/main/java/com/adzteam/urbook/general/ui/rooms/RoomsFragment.java
@@ -1,24 +1,67 @@
 package com.adzteam.urbook.general.ui.rooms;
 
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
 import com.adzteam.urbook.R;
+import com.example.flatdialoglibrary.dialog.FlatDialog;
 
 public class RoomsFragment extends Fragment {
 
-    private RoomsViewModel roomsViewModel;
+    Button mNewRoomBtn;
 
+    @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        roomsViewModel = new ViewModelProvider(this).get(RoomsViewModel.class);
-        //View root = inflater.inflate(R.layout.fragment_rooms, container, false);
-        return inflater.inflate(R.layout.fragment_rooms, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_rooms, container, false);
+
+        mNewRoomBtn = view.findViewById(R.id.addRoom);
+
+        mNewRoomBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showEditDialog();
+            }
+        });
+
+        setHasOptionsMenu(true);
+
+        return view;
+    }
+
+    private void showEditDialog() {
+        final FlatDialog flatDialog = new FlatDialog(getActivity());
+        flatDialog.setTitle("NewRoom")
+                .setBackgroundColor(Color.parseColor("#442D68"))
+                .setFirstButtonColor(Color.parseColor("#F97794"))
+                .setSecondButtonColor(Color.WHITE)
+                .setSecondButtonTextColor(Color.parseColor("#F97794"))
+                .setSubtitle("Write your new room name here, please")
+                .setFirstTextFieldHint("Room Name")
+                .setFirstButtonText("CREATE")
+                .setSecondButtonText("CANCEL")
+                .withFirstButtonListner(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(getActivity(), "The Room " + flatDialog.getFirstTextField() + " was created", Toast.LENGTH_SHORT).show();
+                        flatDialog.dismiss();
+                    }
+                })
+                .withSecondButtonListner(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        flatDialog.dismiss();
+                    }
+                })
+                .show();
     }
 }
