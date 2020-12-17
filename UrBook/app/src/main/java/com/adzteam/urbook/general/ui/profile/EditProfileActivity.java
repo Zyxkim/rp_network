5189f25e1fe4d013231a872d0c8eea747db0552b
zyxkim <zyxkim.bmstu@gmail.com>
2020-12-17 17:38:17 +0300

    Added Edit Profile Activity xml.

---
+++ UrBook/app/src/main/java/com/adzteam/urbook/general/ui/profile/EditProfileActivity.java
@@ -0,0 +1,16 @@
+package com.adzteam.urbook.general.ui.profile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.adzteam.urbook.R;
+
+public class EditProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+    }
+}
