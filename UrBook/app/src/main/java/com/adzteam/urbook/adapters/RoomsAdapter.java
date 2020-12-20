86795257a85bb3be878b45d8b2079d1fbfad2cd4
zyxkim <zyxkim.bmstu@gmail.com>
2020-12-20 11:36:50 +0300

    Added Click On RoomsAdapter Item.

--- UrBook/app/src/main/java/com/adzteam/urbook/adapters/RoomsAdapter.java
+++ UrBook/app/src/main/java/com/adzteam/urbook/adapters/RoomsAdapter.java
@@ -1,43 +1,35 @@
 package com.adzteam.urbook.adapters;
 
-import android.net.Uri;
+import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import android.util.Log;
-import android.widget.Toast;
-
-import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.adzteam.urbook.R;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
-import com.squareup.picasso.Picasso;
+import com.adzteam.urbook.general.GeneralActivity;
+import com.adzteam.urbook.room.RoomActivity;
 
 import java.util.ArrayList;
 
 public class RoomsAdapter extends RecyclerView.Adapter<RoomsAdapter.MyViewHolder> {
 
     private ArrayList<Room> mRoomList;
+    private Context mContext;
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
 
         public TextView mRoomName;
         public TextView mRoomDescription;
-        public ImageView mRoomImg;
 
         public MyViewHolder(View view) {
             super(view);
             mRoomName = (TextView) view.findViewById(R.id.room_name);
             mRoomDescription = (TextView) view.findViewById(R.id.room_description);
-            mRoomImg = (ImageView) view.findViewById(R.id.room_image);
         }
     }
 
@@ -52,13 +44,11 @@ public class RoomsAdapter extends RecyclerView.Adapter<RoomsAdapter.MyViewHolder
         holder.mRoomName.setText(c.getName());
         holder.mRoomDescription.setText(c.getDescription());
 
-        StorageReference mStorageReference = FirebaseStorage.getInstance().getReference();
-        StorageReference profileRef = mStorageReference.child("rooms/" + c.getId() + "/image.jpg");
-
-        profileRef.getDownloadUrl().addOnSuccessListener(new OnSuccessListener<Uri>() {
+        holder.mRoomName.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(Uri uri) {
-                Picasso.get().load(uri).into(holder.mRoomImg);
+            public void onClick(View view) {
+                Intent intent = new Intent(mContext, RoomActivity.class);
+                mContext.startActivity(intent);
             }
         });
     }
@@ -71,6 +61,7 @@ public class RoomsAdapter extends RecyclerView.Adapter<RoomsAdapter.MyViewHolder
 
     @Override
     public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        mContext=parent.getContext();
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.rooms_item, parent, false);
         return new MyViewHolder(v);
     }
