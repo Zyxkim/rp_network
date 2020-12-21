2ceca7aad9159338c951feedfc7bd6f8e9c190cb
zyxkim <zyxkim.bmstu@gmail.com>
2020-12-21 15:01:20 +0300

    Added room deleting.

--- UrBook/app/src/main/java/com/adzteam/urbook/adapters/RoomsAdapter.java
+++ UrBook/app/src/main/java/com/adzteam/urbook/adapters/RoomsAdapter.java
@@ -6,18 +6,24 @@ import android.net.Uri;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.adzteam.urbook.R;
+import com.adzteam.urbook.general.GeneralActivity;
+import com.adzteam.urbook.general.ui.rooms.RoomsFragment;
 import com.adzteam.urbook.room.RoomActivity;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.squareup.picasso.Picasso;
@@ -36,9 +42,11 @@ public class RoomsAdapter extends RecyclerView.Adapter<RoomsAdapter.MyViewHolder
         public TextView mRoomName;
         public TextView mRoomDescription;
         public ImageView mRoomImg;
+        public ImageButton mDeleteBtn;
 
         public MyViewHolder(View view) {
             super(view);
+            mDeleteBtn = view.findViewById(R.id.button_delete);
             mRoomName = (TextView) view.findViewById(R.id.room_name);
             mRoomDescription = (TextView) view.findViewById(R.id.room_description);
             mRoomImg = (ImageView) view.findViewById(R.id.room_image);
@@ -56,6 +64,12 @@ public class RoomsAdapter extends RecyclerView.Adapter<RoomsAdapter.MyViewHolder
         holder.mRoomName.setText(c.getName());
         holder.mRoomDescription.setText(c.getDescription());
 
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        if (c.getCreator().equals(mAuth.getCurrentUser().getUid())) {
+            holder.mDeleteBtn.setVisibility(View.VISIBLE);
+        } else {
+            holder.mDeleteBtn.setVisibility(View.INVISIBLE);
+        }
 
         if (c.isThereImage()) {
             StorageReference mStorageReference = FirebaseStorage.getInstance().getReference();
@@ -80,6 +94,16 @@ public class RoomsAdapter extends RecyclerView.Adapter<RoomsAdapter.MyViewHolder
                 Log.d("ItemClick", CURRENT_ROOM_ID);
                 Intent intent = new Intent(mContext, RoomActivity.class);
                 mContext.startActivity(intent);
+            }
+        });
+
+        holder.mDeleteBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FirebaseFirestore db = FirebaseFirestore.getInstance();
+                db.collection("rooms").document(c.getId()).delete();
+                Toast.makeText(view.getContext(), "Please, refresh page.", Toast.LENGTH_SHORT).show();
+                mRoomList.remove(position);
             }
         });
     }
